936b9e38ce6fdbf62ab96fccab6e03e3cb9b8acf
Christian Duvholt <christian@duvholt.net>
2019-02-12 22:19:38 +0100

    Replace ts-node with babel-node. Works using Nextjs 7

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,15 @@
 import { createServer } from 'http'
 import { parse } from 'url'
-import * as next from 'next'
+import next from 'next'
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
+const app = next({ dev });
 const handle = app.getRequestHandler()
+
+// Just an example that triggers Babel.
+// The problem is not specific to object spread.
+const thisBreaks = {...{}};
 
 app.prepare()
 .then(() => {
